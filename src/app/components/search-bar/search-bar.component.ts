85f82f6cb577054f397f83b0ca5982d4c6feaa57
Sarvar <r0ssmike727@yandex.com>
2022-01-06 20:53:04 +0500

    add game-tabs c

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -5,17 +5,14 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    this.router.navigate(['search'], form.value.search)
+    this.router.navigate(['search', form.value.search]);
   }
-
 }
